e3cf17e3879b4ff576b4e535a8903ab25399a025
Dawid Budzyński <dawidbudzynski@Mac.lan>
2026-08-25 14:50:50 +0200

    perf(select-vars): avoid deep copies in variable selectors
    - get_vars_ind/get_vars_indl/fselect: build named indices and logical
      vectors with assignment syntax instead of direct names<-/[<-/attr<-
      replacement calls
    - get_vars_ind<-: restore the class with oldClass(x) <- before the
      conditional data.table allocation check
    - date_vars: drop redundant attribute stripping before vapply()
    
    See #311.

--- R/select_replace_add_vars.R
+++ R/select_replace_add_vars.R
@@ -5,9 +5,21 @@ get_vars_ind <- function(x, ind, return = "data")
          data = .Call(C_subsetCols, x, ind, TRUE),
          names = attr(x, "names")[ind],
          indices = ind,
-         named_indices = `names<-`(ind, attr(x, "names")[ind]),
-         logical = `[<-`(logical(length(unclass(x))), ind, value = TRUE),
-         named_logical = `names<-`(`[<-`(logical(length(unclass(x))), ind, value = TRUE), attr(x, "names")),
+         named_indices = {
+           names(ind) <- attr(x, "names")[ind]
+           ind
+         },
+         logical = {
+           out <- logical(length(unclass(x)))
+           out[ind] <- TRUE
+           out
+         },
+         named_logical = {
+           out <- logical(length(unclass(x)))
+           out[ind] <- TRUE
+           names(out) <- attr(x, "names")
+           out
+         },
          stop("Unknown return option!"))
 
 # ind must be logical !!! (this used to be get_vars_FUN)
@@ -16,9 +28,15 @@ get_vars_indl <- function(x, indl, return = "data")
          data = .Call(C_subsetCols, x, which(indl), TRUE),
          names = attr(x, "names")[indl],
          indices = which(indl),
-         named_indices = which(`names<-`(indl, attr(x, "names"))),
+         named_indices = {
+           names(indl) <- attr(x, "names")
+           which(indl)
+         },
          logical = indl,
-         named_logical = `names<-`(indl, attr(x, "names")),
+         named_logical = {
+           names(indl) <- attr(x, "names")
+           indl
+         },
          stop("Unknown return option!"))
 
 # ind can be integer or logical
@@ -41,7 +59,8 @@ get_vars_indl <- function(x, indl, return = "data")
     if(length(ind) != 1L) stop("NCOL(value) must match selected variables") # length(num_vars(x))
     x[[ind]] <- value
   }
-  return(condalc(`oldClass<-`(x, clx), any(clx == "data.table")))
+  oldClass(x) <- clx
+  return(condalc(x, any(clx == "data.table")))
 }
 
 
@@ -50,7 +69,8 @@ fselect <- function(.x, ..., return = "data") { # This also takes names and indi
   # oldClass(.x) <- NULL # attributes ?
   nam <- attr(.x, "names")
   # if(inherits(.x, "data.table")) nam <- nam[seq_col(.x)] # required because of overallocation... -> Should be solved now, always take shallow copy...
-  nl <- `names<-`(as.vector(seq_along(nam), "list"), nam)
+  nl <- as.vector(seq_along(nam), "list")
+  names(nl) <- nam
   vars <- eval(substitute(c(...)), nl, parent.frame())
   # if(!is.integer(vars)) stop(paste0("Unknown columns: ", .c(...))) # if(!is.integer(vars) || bmax(vars) > length(nam)) # nah, a bit redundant..
   if(!is.atomic(vars) || is.logical(vars)) stop("... needs to be expressions evaluating to integer or character")
@@ -62,12 +82,27 @@ fselect <- function(.x, ..., return = "data") { # This also takes names and indi
   }
   # if(!is.numeric(vars)) stop("... needs to be column names, or character / integer / logical vectors")
   switch(return, # need this for sf data.frame
-         data = .Call(C_subsetCols, if(length(nam_vars)) `attr<-`(.x, "names", nam) else .x, vars, TRUE), # setAttributes(.x[vars], `[[<-`(ax, "names", nam[vars])), # Also Improvements in code below ?
+         data = {
+           if(length(nam_vars)) attr(.x, "names") <- nam
+           .Call(C_subsetCols, .x, vars, TRUE)
+         },
          names = nam[vars],
          indices = vars,
-         named_indices = `names<-`(vars, nam[vars]),
-         logical = `[<-`(logical(length(nam)), vars, TRUE),
-         named_logical = `names<-`(`[<-`(logical(length(nam)), vars, TRUE), nam),
+         named_indices = {
+           names(vars) <- nam[vars]
+           vars
+         },
+         logical = {
+           out <- logical(length(nam))
+           out[vars] <- TRUE
+           out
+         },
+         named_logical = {
+           out <- logical(length(nam))
+           out[vars] <- TRUE
+           names(out) <- nam
+           out
+         },
          stop("Unknown return option"))
 }
 
@@ -80,7 +115,8 @@ slt <- fselect # good, consistent
 "fselect<-" <- function(x, ..., value) {
   nam <- attr(x, "names")
   # if(inherits(x, "data.table")) nam <- nam[seq_col(x)] # required because of overallocation... Should be solved now -> always make shallow copy
-  nl <- `names<-`(as.vector(seq_along(nam), "list"), nam)
+  nl <- as.vector(seq_along(nam), "list")
+  names(nl) <- nam
   vars <- eval(substitute(c(...)), nl, parent.frame())
   if(!is.atomic(vars) || is.logical(vars)) stop("... needs to be expressions evaluating to integer or character")
   if(is.character(vars)) vars <- ckmatch(vars, nam)
@@ -111,8 +147,8 @@ fact_vars <- function(x, return = "data") get_vars_indl(x, .Call(C_vtypes, x, 2L
 logi_vars <- function(x, return = "data") get_vars_ind(x, .Call(C_vtypes, x, 0L) %==% 11L, return) # vapply(`attributes<-`(x, NULL), is.logical, TRUE)
 "logi_vars<-" <- function(x, value) `get_vars_ind<-`(x, .Call(C_vtypes, x, 0L) %==% 11L, value)
 
-date_vars <- function(x, return = "data") get_vars_indl(x, vapply(`attributes<-`(x, NULL), is_date, TRUE), return)
-"date_vars<-" <- function(x, value) `get_vars_ind<-`(x, vapply(`attributes<-`(x, NULL), is_date, TRUE), value)
+date_vars <- function(x, return = "data") get_vars_indl(x, vapply(x, is_date, TRUE), return)
+"date_vars<-" <- function(x, value) `get_vars_ind<-`(x, vapply(x, is_date, TRUE), value)
 # Date_vars <- function(x, return = "data") {
 #   .Deprecated(msg = "'Date_vars' was renamed to 'date_vars'. It will be removed end of 2023, see help('collapse-renamed').")
 #   date_vars(x, return)
